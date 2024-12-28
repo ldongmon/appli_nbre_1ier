186accf500df7dda225ed71b669e70a1c93c6b85
LEPRINCE DONGMO <leprince.dongmo@yahoo.fr>
2024-12-28 14:21:25 +0100

    premier push

---
+++ appli_nbre_1ier.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def main():
+    st.title("Vérification de Nombre Premier")
+    n = st.number_input("Choisissez un nombre:", min_value=1, step=1)
+    
+    if n == 2:
+        st.success("Ce nombre est premier.")
+    elif n > 2:
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                st.error("Ce nombre n'est pas premier.")
+                break
+        else:
+            st.success("Ce nombre est premier.")
+    else:
+        st.error("Ce nombre n'est pas premier.")
+
+if __name__ == "__main__":
+    main()
